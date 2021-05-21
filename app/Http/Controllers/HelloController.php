1c3170992b1445af47d1816cde786e7c7009579c
takayama shunya <katonbo1@icloud.com>
2021-05-21 17:31:45 +0900

    フォームリクエスト作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -12,18 +13,11 @@ class HelloController extends Controller
        return view('hello.index', ['msg'=>'フォームを入力：']);
    }
 
-
-   public function post(Request $request)
+   public function post(HelloRequest $request)
    {
-       $validate_rule = [
-           'name' => 'required',
-           'mail' => 'email',
-           'age' => 'numeric|between:0,150',
-       ];
-       $this->validate($request, $validate_rule);
-       return view('hello.index', ['msg'=>'正しく入力されました！']);
+      return view('hello.index', ['msg'=>'正しく入力されました！']);
    }
-   
+      
     public function other() {
 
         return view('welcome');
