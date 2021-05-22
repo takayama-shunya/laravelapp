00072f696f82b527fccdda91db08734a80f60bd5
takayama shunya <katonbo1@icloud.com>
2021-05-22 22:14:22 +0900

    バリデーター作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -13,11 +14,20 @@ class HelloController extends Controller
        return view('hello.index', ['msg'=>'フォームを入力：']);
    }
 
-   public function post(HelloRequest $request)
+   public function post(Request $request)
    {
+      $validator = Validator::make($request->all(), [
+          'name' => 'required',
+          'mail' => 'email',
+          'age' => 'numeric|between:0,150',
+      ]);
+      if ($validator->fails()) {
+          return redirect('/hello')
+                      ->withErrors($validator)
+                      ->withInput();
+      }
       return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }
-      
+   }      
     public function other() {
 
         return view('welcome');
