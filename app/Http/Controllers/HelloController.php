0186f8aa09ac78ec761a6a187180c36511dd0c25
takayama shunya <katonbo1@icloud.com>
2021-05-24 20:11:02 +0900

    エラーメッセージカスタマイズ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,24 +15,40 @@ class HelloController extends Controller
    }
 
    public function post(Request $request)
+  {
+   $rules = [
+       'name' => 'required',
+       'mail' => 'email',
+       'age' => 'numeric',
+   ];
+   $messages = [
+       'name.required' => '名前は必ず入力して下さい。',
+       'mail.email'  => 'メールアドレスが必要です。',
+       'age.numeric' => '年齢は整数で記入下さい。',
+       'age.min' => '年齢はゼロ歳以上で記入下さい。',
+       'age.max' => '年齢は200歳以下で記入下さい。',
+   ];
+   $validator = Validator::make($request->all(), $rules, $messages);
+
+   $validator->sometimes('age', 'min:0', function($input){
+       return !is_int($input->age);
+   });
+   $validator->sometimes('age', 'max:200', function($input){
+       return !is_int($input->age);
+   });
+
+   if ($validator->fails()) {
+       return redirect('/hello')
+           ->withErrors($validator)
+           ->withInput();
+   }
+   return view('hello.index', ['msg'=>'正しく入力されました！']);
+  }
+  
+   public function other() 
    {
-      $validator = Validator::make($request->all(), [
-          'name' => 'required',
-          'mail' => 'email',
-          'age' => 'numeric|between:0,150',
-      ]);
-      if ($validator->fails()) {
-          return redirect('/hello')
-                      ->withErrors($validator)
-                      ->withInput();
-      }
-      return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }      
-    public function other() {
+      return view('welcome');
+   }
 
-        return view('welcome');
-    }
-
-   
 }
 
